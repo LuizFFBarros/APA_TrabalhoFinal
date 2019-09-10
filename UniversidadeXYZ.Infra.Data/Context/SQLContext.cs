d8f858b5f8edd6c0c80842b3209b9a193cfebe91
Carina Roriz Pimenta <carinaroriz@hotmail.com>
2019-09-09 22:58:06 -0300

    Ajuste do mapeamento das entidades

--- UniversidadeXYZ.Infra.Data/Context/SQLContext.cs
+++ UniversidadeXYZ.Infra.Data/Context/SQLContext.cs
@@ -10,6 +10,11 @@ namespace UniversidadeXYZ.Infra.Data.Context
     public class SQLContext : DbContext
     {
         public DbSet<Aluno> Aluno { set; get; }
+        public DbSet<Disciplina> Disciplina { set; get; }
+        public DbSet<DisciplinaTurma> DisciplinaTurma { set; get; }
+        public DbSet<Matricula> Matricula { set; get; }
+        public DbSet<Turma> Turma { set; get; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -21,6 +26,10 @@ namespace UniversidadeXYZ.Infra.Data.Context
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Aluno>(new AlunoMap().Configure);
+            modelBuilder.Entity<Disciplina>(new DisciplinaMap().Configure);
+            modelBuilder.Entity<DisciplinaTurma>(new DisciplinaTurmaMap().Configure);
+            modelBuilder.Entity<Matricula>(new MatriculaMap().Configure);
+            modelBuilder.Entity<Turma>(new TurmaMap().Configure);
         } 
     }
 }
---
+++ UniversidadeXYZ.Infra.Data/Mapping/DisciplinaTurmaMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversidadeXYZ.Dominio.Entidades;
+
+namespace UniversidadeXYZ.Infra.Data.Mapping
+{
+    public class DisciplinaTurmaMap : IEntityTypeConfiguration<DisciplinaTurma>
+    {
+        public void Configure(EntityTypeBuilder<DisciplinaTurma> builder)
+        {
+            builder.ToTable("DisciplinaTurma")
+                    .HasKey(a => new { a.CodigoDisciplina, a.CodigoDaTurma });
+
+            builder.Property(a => a.QuantidadeVagas)
+                   .IsRequired();
+
+            builder.HasOne(a => a.Disciplina)
+                .WithMany(b => b.DisciplinaTurma)
+                .HasForeignKey(a => a.CodigoDisciplina);
+
+            builder.HasOne(a => a.Turma)
+                .WithMany(b => b.DisciplinaTurma)
+                .HasForeignKey(a => a.CodigoDaTurma);
+
+            builder.HasMany(a => a.Matricula)
+                   .WithOne(b => b.DisciplinaTurma);
+        }
+    }
+}
