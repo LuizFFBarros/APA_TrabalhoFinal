63a8b1426d915cc9d7efd77d6acc4b395c134b8f
Luiz <figueiredobarros@gmail.com>
2019-09-25 00:59:07 -0300

    inicio de matricula tela

---
+++ UniversidadeXYZ.Web/Models/MatriculaAdicionarModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversidadeXYZ.Web.Models
+{
+    public class MatriculaAdicionarModel
+    {
+        public int CodigoAluno { get; set; }
+        public SelectList ListaDisciplinaTurma { get; set; }
+
+    }
+}
