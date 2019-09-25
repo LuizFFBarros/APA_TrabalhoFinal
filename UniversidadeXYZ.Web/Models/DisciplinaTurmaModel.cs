3e0e283d5e4f2b02ef4eb0e204d185a2ddfc93ed
Carina Roriz Pimenta <carinaroriz@hotmail.com>
2019-09-24 23:09:00 -0300

    Adicionando tela de visualização das matrículas do aluno

---
+++ UniversidadeXYZ.Web/Models/DisciplinaTurmaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversidadeXYZ.Web.Models
+{
+    public class DisciplinaTurmaModel
+    {
+        public DisciplinaModel Disciplina { get; set; }
+        public int CodigoDisciplina { get; set; }
+        public int CodigoDaTurma { get; set; }
+        public decimal QuantidadeVagas { get; set; }
+    }
+}
